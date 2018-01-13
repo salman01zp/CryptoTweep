6b029d11e1db3e76f2313fa8cda76c1e0e4e5c9f
Salman Pathan <pathansalman555@gmail.com>
2018-01-13 11:45:52 +0530

    create credential file
    replace credential file details with your own twitter api keys

---
+++ credential.py
@@ -0,0 +1,4 @@
+consumer_key = '0b7NpXrlo*****8cviX7Ehl'
+consumer_secret = 'vXT3a8woFb*********VCuB24ZlCdcJJ7CxiDlbhw8e5wN'
+access_token = '931724058445492224-r********9MZGMBM2odTlXu90tLbQP'
+access_token_secret = 'ERbB8DhnDBhk********DqgQBbOlOQTGXIM5Zp'
